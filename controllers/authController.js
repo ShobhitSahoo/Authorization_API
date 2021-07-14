68a0e4e78d630a775deb37d7d08521a332b86d48
Shobhit Preetam Sahoo <shobhitsahoo46000@gmail.com>
2021-07-14 19:07:11 +0530

    updating user features

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,6 +11,18 @@ const signToken = id => {
     return jwt.sign({ id: id }, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN});
 };
 
+const createSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id);
+
+    res.status(statusCode).json({
+        status: 'success',
+        token,
+        data: {
+            user
+        }
+    });
+};
+
 exports.signup = catchAsync(async (req, res, next) => {
 
     const newUser = await User.create({
@@ -20,15 +32,16 @@ exports.signup = catchAsync(async (req, res, next) => {
         passwordConfirm: req.body.passwordConfirm
     });
 
-    const token = signToken(newUser._id);
+    createSendToken(newUser, 201, res);
+    // const token = signToken(newUser._id);
 
-    res.status(201).json({
-        status: 'success',
-        token,
-        data: {
-            user: newUser
-        }
-    });
+    // res.status(201).json({
+    //     status: 'success',
+    //     token,
+    //     data: {
+    //         user: newUser
+    //     }
+    // });
 });
 
 exports.login = catchAsync( async (req, res, next) => {
@@ -47,12 +60,13 @@ exports.login = catchAsync( async (req, res, next) => {
     }
 
     // 3) If user exists and password is correct, then send token to client
-    const token = signToken(user._id);
+    createSendToken(user, 200, res);
+    // const token = signToken(user._id);
 
-    res.status(200).json({
-        status: 'success',
-        token
-    });
+    // res.status(200).json({
+    //     status: 'success',
+    //     token
+    // });
 });
 
 exports.protect = catchAsync( async (req, res, next) => {
@@ -162,11 +176,33 @@ exports.resetPassword = catchAsync( async (req, res, next) => {
 
 
     // 4) Log the user in, send JWT
-    const token = signToken(user._id);
+    createSendToken(user, 200, res);
 
-    res.status(200).json({
-        status: 'success',
-        token
-    });
+    // const token = signToken(user._id);
+
+    // res.status(200).json({
+    //     status: 'success',
+    //     token
+    // });
+
+});
+
+exports.updatePassword = catchAsync( async ( req, res, next ) => {
+    
+    // 1) Get the user from the password provided.
+    const user = await User.findById(req.user.id).select('+password');
+
+    // 2) Check if provided password is correct or not.
+    if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) 
+        return next(new AppError('Your current password is incorrect.', 401));
+
+    // 3) If correct, update the current password.
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    // Using save() because middlewares are defined on save.
+    await user.save();
+
+    // 4) Log the user in and send JWT
+    createSendToken(user, 200, res);
 
 });
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,14 @@ const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if(allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
 
@@ -12,6 +20,30 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     data: {
       users,
     },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+
+  // 1) Create error if user posts password data
+  if(req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('This route is not for updating passwords. Please use /updateMyPassword for that.', 400));
+  }
+
+  // 2) Filtering out field allowed names from request
+  const filteredData = filterObj(req.body, 'name', 'email');
+
+  // 3) Update user data
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredData, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: updatedUser
+    }
   });
 });
 
