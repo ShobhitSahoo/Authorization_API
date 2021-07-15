e6aa0a34865df738670beef4f44e83701e72c986
Shobhit Preetam Sahoo <shobhitsahoo46000@gmail.com>
2021-07-15 22:30:57 +0530

    another typo

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const crypto = require('crypto');
 
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../Utils/appError');
+const AppError = require('./../utils/appError');
 const Email = require('./../utils/email');
 
 const signToken = id => {
